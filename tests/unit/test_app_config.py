dccd947559cff3976e0a44e5b1c54eddce16d537
Dogan Narinc <hoptek@protonmail.com>
2022-10-04 11:29:14 +0200

    Improve app config unit test so that it can be run in combination with other tests that also import the actual AppConfig class

--- tests/unit/test_app_config.py
+++ tests/unit/test_app_config.py
@@ -1,5 +1,8 @@
 import os
+import sys
 from unittest import TestCase, mock
+
+from python_cli_skeleton_app.app_config import AppConfig
 
 
 class TestAppConfig(TestCase):
@@ -8,6 +11,8 @@ class TestAppConfig(TestCase):
     @mock.patch.dict(os.environ, {}, clear=True)
     def test_raises_error_when_app_env_not_set(self):
         with self.assertRaises(RuntimeError) as error:
+            # ensure that AppConfig will be reloaded on import so that we can test an error being raised
+            del sys.modules['python_cli_skeleton_app.app_config']
             # the line below should raise an error
             from python_cli_skeleton_app.app_config import AppConfig
             # this call should not happen
@@ -18,11 +23,9 @@ class TestAppConfig(TestCase):
         )
 
     def test_returns_expected_string_value(self):
-        from python_cli_skeleton_app.app_config import AppConfig
         actual_test_value = AppConfig.get("test_key_string")
         self.assertEqual("test_value_string", actual_test_value)
 
     def test_returns_expected_int_value(self):
-        from python_cli_skeleton_app.app_config import AppConfig
         actual_test_value = AppConfig.getint("test_key_int")
         self.assertEqual(42, actual_test_value)
