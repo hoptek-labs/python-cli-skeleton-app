b36623d88e1f84d8f927a675cac82e21fda2e703
Dogan Narinc <dogan.narinc+code@gmail.com>
2022-11-15 12:51:01 +0100

    Encapsulate business logic in main.py in a dedicated function

--- python_cli_skeleton_app/__main__.py
+++ python_cli_skeleton_app/__main__.py
@@ -6,11 +6,17 @@ from python_cli_skeleton_app.endpoint_caller import call_endpoint
 
 logger = AppLogger.getLogger()
 
-try:
-    logger.info(f"Using APP_ENV {os.getenv('APP_ENV')}")
-    endpoint_url = AppConfig.get("endpoint_url")
-    status_code = call_endpoint()
-    logger.info(f"Endpoint URL {endpoint_url} returned status code {status_code}")
-except Exception as exc:
-    logger.exception(exc)
-    exit(1)
+
+def run():
+    try:
+        logger.info(f"Using APP_ENV {os.getenv('APP_ENV')}")
+        endpoint_url = AppConfig.get("endpoint_url")
+        status_code = call_endpoint()
+        logger.info(f"Endpoint URL {endpoint_url} returned status code {status_code}")
+    except Exception as exc:
+        logger.exception(exc)
+        exit(1)
+
+
+if __name__ == "__main__":
+    run()
